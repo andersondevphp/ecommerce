cc0db78f005624c7059b26d4c683e392df8af380
Anderson Carvalho <andersoninovaarte@gmail.com>
2019-12-10 12:48:12 -0300

    criando PageADMIN

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 $app = new Slim();
 $app->config('debug', true);
+// Template.
 $app->get('/', function() {
 	$page = new Page();
+	$page->setTpl("index");
+});
+// Admin.
+$app->get('/admin', function() {
+	$page = new PageAdmin();
 	$page->setTpl("index");
 });
 $app->run();
